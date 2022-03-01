5abefce4ee5b547d328424fec84a60a4eeaa91ea
Joshua Stresing <pyrotorch69@yahoo.com>
2022-02-28 18:00:47 -0800

    worked on most of the communication of the database

--- break-tracker/break-tracker.js
+++ break-tracker/break-tracker.js
@@ -1,11 +1,16 @@
-import { logout, checkAuth } from '../fetch-utils.js';
+import { logout, checkAuth, createBreak } from '../fetch-utils.js';
 
 checkAuth();
 
 const newBreak = document.getElementById('new-break');
 
-newBreak.addEventListener('click', () => {
-    window.location.replace('/break-menu');
+newBreak.addEventListener('click', async () => {
+    const response = await createBreak();
+    const params = new URLSearchParams();
+    params.set('id', response.id);
+
+    console.log(response);
+    window.location.replace(`/break-menu/?${ params.toString() }`);
 });
 
 const logoutButton = document.getElementById('logout');
