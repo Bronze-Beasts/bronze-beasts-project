7191263b3d524baa92e4f3f026e22add1d5785ab
Joshua Stresing <pyrotorch69@yahoo.com>
2022-03-03 14:16:18 -0800

    fix error button and functions

--- break-tracker/break-tracker.js
+++ break-tracker/break-tracker.js
@@ -113,14 +113,41 @@ async function displayAdvice() {
 
 // displayAdvice();
 
-errorButton.addEventListener('click', async ()=> {
+errorButton.addEventListener('click', (gotError));
+
+async function gotError(){  
     displayAdvice();
     // setInterval(displayAdvice);
-    errorButton.disabled = true;
+    errorButton.textContent = 'Fixed your Error?';
     errorTimer.classList.remove('hidden');
+    errorButton.removeEventListener('click', gotError);
+    errorButton.addEventListener('click', fixedError);
     errorTimerStart();
-    
-});
+
+} 
+
+async function fixedError() {
+    errorTimer.classList.add('hidden');
+    errorButton.removeEventListener('click', fixedError);
+    errorButton.addEventListener('click', gotError);
+    errorButton.textContent = 'Have an Error?';
+    adviceSection.textContent = '';
+    resetTimer();
+    console.log('fixed error');
+}
+
+function resetTimer() {
+
+    let min = 15;
+    let sec = 0;
+
+    let appendErrorMin = document.getElementById('errorMinutes');
+    let appendErrorSec = document.getElementById('errorSeconds');
+
+    appendErrorMin.textContent = '15'
+    appendErrorSec.textContent = '00'
+
+}
 
 function errorTimerStart() {
 
